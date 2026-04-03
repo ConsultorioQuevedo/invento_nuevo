9ecef39e3dfde2c3dba70cb60d069c11b22a6d2a
ConsultorioQuevedo <luisraqueal@gmail.com>
2026-04-03 19:09:27 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# Título de la aplicación
+st.title("invento")
+
+# Mensaje de bienvenida
+st.write("Bienvenido a la solucion.")
+
+# Sección de Créditos al pie de página
+st.markdown("---")
+st.caption("Diseñadores: Luis Rafael Quevedo")
